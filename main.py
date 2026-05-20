b8cc087d63127cbe732e0415eb3f7df5c379e840
HariprasathS-3011 <hariprasathselvam3011@gmail.com>
2026-05-20 14:58:24 +0530

    second commit

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ Email=input("Enter the email_id ")
 print(name)
 print(age)
 print(Email)
+# =================First logic ====================
+if age>=18:
+    print("you can access the ATM machine")
+else: 
+    print("you cannot access the ATM machine")
+    
