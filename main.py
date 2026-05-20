85c82baa2254ce290dcd54d1745a2d1a41024b75
Vasanth J <jkvasanth24@gmail.com>
2026-05-20 15:07:44 +0530

    Check balance

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ print(name)
 print(age)
 print(Email)
 # =================First logic ====================
+bal = 10000
 if age>=18:
     print("you can access the ATM machine")
+    print("1.Checkbalance")
+    print("2.Deposit")
+    print("3.Withdrawal")
+    print("4.Exit")
+    match bal:
+        case 1:
+            print("Your balance is: "+bal)
+            
 else: 
     print("you cannot access the ATM machine")
     
